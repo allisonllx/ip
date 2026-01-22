0bd37b6147316c7baa3f4b44f230ffc275fcc43a
Allison Law Li Xuan <allisonlixuan@gmail.com>
2026-01-22 14:50:44 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Allison.java
@@ -0,0 +1,9 @@
+public class Allison {
+    public static void main(String[] args) {
+        System.out.println("-".repeat(60));
+        System.out.println("Hello! I'm Allison.\nWhat can I do for you?");
+        System.out.println("-".repeat(60));
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-".repeat(60));
+    }
+}
