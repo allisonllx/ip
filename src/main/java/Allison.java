aa9ff7226bc45d49e18e38cea36f0ed22bf82683
Allison Law Li Xuan <allisonlixuan@gmail.com>
2026-01-22 17:51:17 +0800

    Automated Text UI Testing

--- src/main/java/Allison.java
+++ src/main/java/Allison.java
@@ -6,35 +6,38 @@ public class Allison {
         Task[] list = new Task[100];
         int counter = 0;
 
-        System.out.println("-".repeat(60));
+        System.out.println("_".repeat(60));
         System.out.println("Hello! I'm Allison.\nWhat can I do for you?");
-        System.out.println("-".repeat(60));
+        System.out.println("_".repeat(60));
 
         String text = "";
         while (!text.trim().equalsIgnoreCase("bye")) { // loop until user types "bye"
-            System.out.print("> "); // prompt for input
+//            System.out.print("> "); // prompt for input
             text = sc.nextLine();
 
             if (text.trim().equalsIgnoreCase("bye")) {
-                System.out.println("-".repeat(60));
+                System.out.println("_".repeat(60));
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("-".repeat(60));
+                System.out.print("_".repeat(60));
             } else if (text.trim().equalsIgnoreCase("list")) {
-                System.out.println("-".repeat(60));
+                System.out.println("_".repeat(60));
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < list.length; i++) {
                     if (list[i] != null) {
                         System.out.println(i+1 + ". " + list[i]);
                     }
                 }
+                System.out.println("_".repeat(60));
             } else if (text.toLowerCase().startsWith("mark")) {
                 String[] parts = text.split(" ");
                 if (parts.length == 2) {
                     try {
                         int taskNumber = Integer.parseInt(parts[1]); // convert string to int
+                        System.out.println("_".repeat(60));
                         System.out.println("Nice! I've marked this task as done:");
                         list[taskNumber - 1].markAsDone();
                         System.out.println(list[taskNumber - 1]);
+                        System.out.println("_".repeat(60));
                     } catch (NumberFormatException e) {
                         System.out.println("Invalid number after 'mark'.");
                     }
@@ -46,9 +49,11 @@ public class Allison {
                 if (parts.length == 2) {
                     try {
                         int taskNumber = Integer.parseInt(parts[1]); // convert string to int
+                        System.out.println("_".repeat(60));
                         System.out.println("OK, I've marked this task as not done yet:");
                         list[taskNumber - 1].markAsUndone();
                         System.out.println(list[taskNumber - 1]);
+                        System.out.println("_".repeat(60));
                     } catch (NumberFormatException e) {
                         System.out.println("Invalid number after 'unmark'.");
                     }
@@ -67,11 +72,11 @@ public class Allison {
                 }
                 list[counter++] = todo;
 
-                System.out.println("-".repeat(60));
+                System.out.println("_".repeat(60));
                 System.out.println("Got it. I've added this task:");
                 System.out.println(todo);
                 System.out.println("Now you have " + counter + " tasks in the list.");
-                System.out.println("-".repeat(60));
+                System.out.println("_".repeat(60));
             } else if (text.toLowerCase().startsWith("deadline")) {
                 if (!text.contains("/by")) {
                     System.out.println("Usage: deadline <task> /by <time>");
@@ -92,11 +97,11 @@ public class Allison {
                 }
                 list[counter++] = deadline;
 
-                System.out.println("-".repeat(60));
+                System.out.println("_".repeat(60));
                 System.out.println("Got it. I've added this task:");
                 System.out.println(deadline);
                 System.out.println("Now you have " + counter + " tasks in the list.");
-                System.out.println("-".repeat(60));
+                System.out.println("_".repeat(60));
             } else if (text.toLowerCase().startsWith("event")) {
                 if (!text.contains("/from") || !text.contains("/to")) {
                     System.out.println("Usage: event <desc> /from <start> /to <end>");
@@ -114,15 +119,15 @@ public class Allison {
                 Event event = new Event(description, from, to);
                 list[counter++] = event;
 
-                System.out.println("-".repeat(60));
+                System.out.println("_".repeat(60));
                 System.out.println("Got it. I've added this task:");
                 System.out.println(event);
                 System.out.println("Now you have " + counter + " tasks in the list.");
-                System.out.println("-".repeat(60));
+                System.out.println("_".repeat(60));
             } else {
-                System.out.println("-".repeat(60));
+                System.out.println("_".repeat(60));
                 System.out.println("Invalid command");
-                System.out.println("-".repeat(60));
+                System.out.println("_".repeat(60));
             }
         }
 
