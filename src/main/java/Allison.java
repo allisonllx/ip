e3956bb7ecaaa0e80cb509fa5d53f51f3a39bbdf
Allison Law Li Xuan <allisonlixuan@gmail.com>
2026-01-22 15:09:24 +0800

    Level 2. Add, List

--- src/main/java/Allison.java
+++ src/main/java/Allison.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Allison {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] list = new String[100];
+        int counter = 0;
 
         System.out.println("-".repeat(60));
         System.out.println("Hello! I'm Allison.\nWhat can I do for you?");
@@ -17,9 +19,17 @@ public class Allison {
                 System.out.println("-".repeat(60));
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("-".repeat(60));
-            } else {
+            } else if (text.equalsIgnoreCase("list")) {
                 System.out.println("-".repeat(60));
-                System.out.println(text);
+                for (int i = 0; i < list.length; i++) {
+                    if (list[i] != null) {
+                        System.out.println(i+1 + ". " + list[i]);
+                    }
+                }
+            } else {
+                list[counter++] = text;
+                System.out.println("-".repeat(60));
+                System.out.println("added: " + text);
                 System.out.println("-".repeat(60));
             }
         }
